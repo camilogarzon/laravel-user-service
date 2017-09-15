384cbeb6f52907f694c97bf58901955437bfd9b8
camilogarzon <camilogc2@gmail.com>
2017-09-15 01:09:00 -0400

    schedule a task

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        \App\Console\Commands\UpdateAccountKeysCommand::class,
     ];
 
     /**
@@ -24,8 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command('service:update-account-keys')->everyTenMinutes();
     }
 
     /**
